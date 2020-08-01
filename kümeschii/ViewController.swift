5416eb6ab7a97461797fe46f8b523d5ea9910293
tunahannn123 <41261009+tunahannn123@users.noreply.github.com>
2020-08-01 15:59:28 +0300

    Implameted image detection

--- kümeschii/ViewController.swift
+++ kümeschii/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Networking code here")
+        print("Image detection")
     }
 
 
